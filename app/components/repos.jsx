414b8ec315c52afc53d9b729d191989f037dbebe
Toshiyuki HINA <toshiyuki.hina@gmail.com>
2016-05-16 18:35:03 +0900

    Add 'About' and 'Repos' pages

---
+++ app/components/repos.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+export default class Repos extends React.Component {
+
+  constructor(props) {
+    super(props);
+  }
+
+  render() {
+    return (<div>Repos</div>);
+  }
+
+}
--- app/index.jsx
+++ app/index.jsx
@@ -2,13 +2,17 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import { Router, Route, hashHistory } from 'react-router';
 
-require('../node_modules/bootstrap/dist/css/bootstrap.css')
+require('../node_modules/bootstrap/dist/css/bootstrap.css');
 
-import App from './components/app'
+import App from './components/app';
+import About from './components/about';
+import Repos from './components/repos';
 
 ReactDOM.render((
     <Router history={hashHistory}>
       <Route path="/" component={App} />
+      <Route path="/repos" component={Repos} />
+      <Route path="/about" component={About} />
     </Router>
   ),
   document.body.appendChild(document.createElement('div'))
