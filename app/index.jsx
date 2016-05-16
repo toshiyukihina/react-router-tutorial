d7bc553f8e0018d220fbb4bf845c3e97c45b2137
Toshiyuki HINA <toshiyuki.hina@gmail.com>
2016-05-16 18:22:47 +0900

    Install react-router and set App copoment to '/'

--- app/index.jsx
+++ app/index.jsx
@@ -1,10 +1,15 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { Router, Route, hashHistory } from 'react-router';
+
 require('../node_modules/bootstrap/dist/css/bootstrap.css')
 
 import App from './components/app'
 
-ReactDOM.render(
-  <App />,
+ReactDOM.render((
+    <Router history={hashHistory}>
+      <Route path="/" component={App} />
+    </Router>
+  ),
   document.body.appendChild(document.createElement('div'))
 );
