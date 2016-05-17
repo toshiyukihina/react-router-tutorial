9ff71108d7f494d9fb8571df8e6f2277a87f7c77
Toshiyuki HINA <toshiyuki.hina@gmail.com>
2016-05-17 12:36:26 +0900

    Add a navi link wrapper

--- app/components/app.jsx
+++ app/components/app.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Link } from 'react-router';
+import NavLink from './nav_link'
 
 class App extends React.Component {
 
@@ -12,8 +12,8 @@ class App extends React.Component {
       <div>
         <h1>React Router Tutorial</h1>
         <ul role="nav">
-          <li><Link to="/about" activeClassName="active">About</Link></li>
-          <li><Link to="/repos" activeClassName="active">Repos</Link></li>
+          <li><NavLink to="/about">About</NavLink></li>
+          <li><NavLink to="/repos">Repos</NavLink></li>
         </ul>
         { this.props.children }
       </div>
