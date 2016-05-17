367959d2a2fc0245f9ded1b9d6f141584b67ffff
Toshiyuki HINA <toshiyuki.hina@gmail.com>
2016-05-17 13:18:29 +0900

    Add 'Repo' and some links to show the repo

--- app/index.jsx
+++ app/index.jsx
@@ -8,11 +8,13 @@ require('./index.css');
 import App from './components/app';
 import About from './components/about';
 import Repos from './components/repos';
+import Repo from './components/repo'
 
 ReactDOM.render((
     <Router history={hashHistory}>
       <Route path="/" component={App}>
         <Route path="/repos" component={Repos} />
+        <Route path="/repos/:userName/:repoName" component={Repo} />
         <Route path="/about" component={About} />
       </Route>
     </Router>
