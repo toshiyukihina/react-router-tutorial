f2dd80bddaaeb6b91729b85001139450bb825e3c
Toshiyuki HINA <toshiyuki.hina@gmail.com>
2016-05-17 16:53:37 +0900

    Use browserHistory instead of hashHistory

--- app/index.jsx
+++ app/index.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { Router, Route, hashHistory, IndexRoute } from 'react-router';
+import { Router, Route, IndexRoute, browserHistory } from 'react-router';
 
 import App from './components/app';
 import About from './components/about';
@@ -12,7 +12,7 @@ require('../node_modules/bootstrap/dist/css/bootstrap.css');
 require('./index.css');
 
 ReactDOM.render((
-  <Router history={hashHistory}>
+  <Router history={browserHistory}>
     <Route path="/" component={App}>
       <IndexRoute component={Home} />
       <Route path="/repos" component={Repos}>
