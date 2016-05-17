f0f0f5f860428fe7489600349efb4ea41db802cb
Toshiyuki HINA <toshiyuki.hina@gmail.com>
2016-05-17 15:41:06 +0900

    Add 'Home' component and render if the URL is '/'

--- app/components/app.jsx
+++ app/components/app.jsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import NavLink from './nav_link'
+import Home from './home'
 
 class App extends React.Component {
 
@@ -15,7 +16,7 @@ class App extends React.Component {
           <li><NavLink to="/about">About</NavLink></li>
           <li><NavLink to="/repos">Repos</NavLink></li>
         </ul>
-        { this.props.children }
+        { this.props.children || <Home /> }
       </div>
     );
   }
