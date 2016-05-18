cd4c03a31ad5b11c9b0e0a83f99a5bc06314b72c
Toshiyuki Hina <toshiyuki.hina@gmail.com>
2016-05-18 10:07:54 +0900

    Delete an unnecessary file

--- app/components/about.jsx
+++ app/components/about.jsx
@@ -1,14 +1,1 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
 
-export default class About extends React.Component {
-
-  constructor(props) {
-    super(props);
-  }
-  
-  render() {
-    return (<div>About</div>);
-  }
-
-}
