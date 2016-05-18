38f6db87e0105a5d659d584b2b5316d9b1ee1c12
Toshiyuki HINA <toshiyuki.hina@gmail.com>
2016-05-18 12:17:42 +0900

    Use react-bootstrap

--- app/components/App.jsx
+++ app/components/App.jsx
@@ -1,23 +1,42 @@
 import React from 'react';
 import { IndexLink } from 'react-router';
+import { Grid, Nav, NavItem, Col } from 'react-bootstrap';
+import { LinkContainer, IndexLinkContainer } from 'react-router-bootstrap';
 import NavLink from './NavLink';
 
 class App extends React.Component {
 
   constructor(props) {
     super(props);
+
+    this.handleSelect = this.handleSelect.bind(this);
+  }
+
+  handleSelect(eventKey) {
+    console.log(`* handleSelect: ${eventKey}`);
   }
 
   render() {
     return (
       <div>
-        <h1>React Router Tutorial</h1>
-        <ul role="nav">
-          <li><IndexLink to="/" activeClassName="active">Home</IndexLink></li>
-          <li><NavLink to="/about">About</NavLink></li>
-          <li><NavLink to="/repos">Repos</NavLink></li>
-        </ul>
-        { this.props.children }
+        <Grid>
+          <Col md={4}>
+          <Nav bsStyle="pills" stacked activeKey={1} onSelect={this.handleSelect}>
+            <IndexLinkContainer to={{ pathname: "/" }}>
+              <NavItem eventKey={1}>Home</NavItem>
+            </IndexLinkContainer>
+            <LinkContainer to={{ pathname: "/about" }}>
+              <NavItem eventKey={2}>About</NavItem>
+            </LinkContainer>
+            <LinkContainer to={{ pathname: "/repos" }}>
+              <NavItem eventKey={3}>Repos</NavItem>
+            </LinkContainer>
+          </Nav>
+          </Col>
+          <Col md={8}>
+            { this.props.children }
+          </Col>
+        </Grid>
       </div>
     );
   }
